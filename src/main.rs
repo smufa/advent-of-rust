f4236b8cd22e164651f1e9aceb2985702b982135
enei <enei.sluga@email.com>
2023-02-02 20:10:45 +0100

    Hello world

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-mod day1;
+// mod day1;
 
-#[path = "utils.rs"]
-mod utils;
+// #[path = "utils.rs"]
+// mod utils;
 
 fn main() {
-    let puzzle = utils::read("./input1");
-    println!("Solution to first part: {}", puzzle.day1::first());
-    println!("Solution to second part: {}", puzzle.second());
+    // let puzzle = utils::read("./input1");
+    // println!("Solution to first part: {}", puzzle.day1::first());
+    // println!("Solution to second part: {}", puzzle.second());
+    println!("Hello world");
 }
